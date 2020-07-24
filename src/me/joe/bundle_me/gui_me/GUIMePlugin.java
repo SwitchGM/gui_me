8f6dd5c59e26ef82ec33055338692d355656ab3c
Switch <32131859+SwitchGM@users.noreply.github.com>
2020-07-24 23:34:20 +0100

    added .gitignore and basic GUIMePlugin.java

---
+++ src/me/joe/bundle_me/gui_me/GUIMePlugin.java
@@ -0,0 +1,11 @@
+package me.joe.bundle_me.gui_me;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GUIMePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("nothing much to see here yet!");
+    }
+}
